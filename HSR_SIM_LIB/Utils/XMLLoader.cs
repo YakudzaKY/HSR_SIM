e5ee33e2fd65b2286b7c774823971176fe5fa0e6
Yakudza_KY <Tehnik89@gmail.com>
2024-02-11 12:08:29 +0300

    scenario name when some profiles selected #69

--- HSR_SIM_LIB/Utils/XMLLoader.cs
+++ HSR_SIM_LIB/Utils/XMLLoader.cs
@@ -31,7 +31,7 @@ public static class XMLLoader
         var xRoot = xDoc.DocumentElement;
         if (xRoot != null)
         {
-            Combat.CurrentScenario.Name = $"{xRoot.Attributes.GetNamedItem("name").Value} ({Path.GetFileNameWithoutExtension(scenarioPath)})" ;
+            Combat.CurrentScenario.Name = $"{xRoot.Attributes.GetNamedItem("name").Value} scenario: {Path.GetFileNameWithoutExtension(scenarioPath)} profile: {Path.GetFileNameWithoutExtension(profilePath)}" ;
 
             //parse all items
             foreach (XmlElement xnode in xRoot)
